0ab74330bf455a9d5019251d07183f8031149882
Edddo123 <edo.miraqian@gmail.com>
2020-12-13 03:14:07 +0400

    Setting up server and other basics

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use((req, res, next)=> {
+    res.render('index')
+})
+
+
+app.listen(3000)
+
